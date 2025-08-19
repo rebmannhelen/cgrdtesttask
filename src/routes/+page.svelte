d7be94c92d778ab617107a305b3b20753b5c4151
hrebmann <helen.rebmann@horze.com>
2025-08-19 16:17:38 +0200

    github test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
+	/*
 	import Login from './components/login.svelte';
 	import Newsfeed from './components/newsfeed.svelte';
 
-	let falseAttempt = false
-	let login = false
-	
-	export let form:any
-	export let data:any
+
+	let falseAttempt = false;
+	let login = false;
+
+	export let form: any;
+	export let data: any;
 
 	$: {
-		if (form?.validUser == false) falseAttempt = true
-		if (form?.validUser == true) login = true
-		if (form?.logout == true) login = false
+		if (form?.validUser == false) falseAttempt = true;
+		if (form?.validUser == true) login = true;
+		if (form?.logout == true) login = false;
 	}
-
+		*/
 </script>
 
-<div class="background">
-	<div class="content">
-		<div class="logo">
-			<img src="/logo.svg" alt="logo" />
-		</div>
-		{#if !login}
+<!--
+	<div class="background">
+		<div class="content">
+			<div class="logo">
+				<img src="/logo.svg" alt="logo" />
+			</div>
+			{#if !login}
 			<Login {falseAttempt}/>
-		{:else}
+			{:else}
 			<Newsfeed {data} {form}/>
-		{/if}
+			{/if}
+		</div>
 	</div>
-</div>
+-->
+
+<div>Kann ich gefunden werden? </div>
