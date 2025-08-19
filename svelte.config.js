eba8433e0e5aa07d1ed4624100dd0dcbf35c4cd5
hrebmann <helen.rebmann@horze.com>
2025-08-19 16:39:32 +0200

    prerender

--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,9 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
+		prerender: {
+			entries: ['*'] // prerender all pages
+		},
 		paths: {
 			base: process.argv.includes('dev') ? '' : '/cgrdtesttask'
 		}
